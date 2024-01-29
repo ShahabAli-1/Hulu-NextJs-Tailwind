e2da910f46b120722e1aa6070f9089d771b667d8
Eisha Fatima <efatima.bese20seecs@gmail.com>
2024-01-29 21:39:59 +0500

    Final Code of the Hulu Project

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    domains: [
+      "www.clipartmax.com",
+      "www.google.com",
+      "links.papareact.com",
+      "image.tmdb.org",
+      "deadline.com",
+    ],
+  },
+};
